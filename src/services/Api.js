4ab3337bfa6cfe45a4c0f076bedbec4ba6202be3
jjzcru <jjzcru@gmail.com>
2020-12-22 23:38:28 -0500

    feat: :construction: Add Me component
    Add component that display user data

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,78 @@ export default class Api {
 		this.host = 'https://coding-test.rootstack.net';
 	}
 	getJobs() {
-		return new Promise({});
+		return new Promise(async (resolve, reject) => {
+			try {
+				const token = localStorage.getItem('token');
+				const response = await fetch(`${this.host}/api/jobs`, {
+					method: 'get',
+					mode: 'cors',
+					redirect: 'follow',
+					referrer: 'no-referrer',
+					headers: {
+						Accept: '*/*',
+						Authorization: `bearer ${token}`,
+					},
+				});
+				const { last_page } = await response.json();
+				if (!last_page) {
+					reject(new Error('INVALID_TOKEN'));
+					return;
+				}
+
+				resolve(await this.getJobsFromAllThePages(last_page));
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	getJobsFromAllThePages = async (totalOfPages) => {
+		const jobs = [];
+		const promises = [];
+		for (let i = 1; i <= totalOfPages; i++) {
+			promises.push(
+				new Promise(async (resolve, reject) => {
+					try {
+                        const jobsFromPage = await this.getJobsByPage(i);
+                        for(const job of jobsFromPage) {
+                            jobs.push(job);
+                        }
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				})
+			);
+		}
+
+		await Promise.all(promises);
+		return jobs;
+	};
+
+	getJobsByPage(page) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const token = localStorage.getItem('token');
+				const response = await fetch(
+					`${this.host}/api/jobs?page=${page}`,
+					{
+						method: 'get',
+						mode: 'cors',
+						redirect: 'follow',
+						referrer: 'no-referrer',
+						headers: {
+							Accept: '*/*',
+							Authorization: `bearer ${token}`,
+						},
+					}
+				);
+				const { data } = await response.json();
+				resolve(data);
+			} catch (e) {
+				reject(e);
+			}
+		});
 	}
 
 	getMe() {
@@ -16,8 +87,8 @@ export default class Api {
 					redirect: 'follow',
 					referrer: 'no-referrer',
 					headers: {
-                        Accept: '*/*',
-                        Authorizatio: `bearer ${token}`
+						Accept: '*/*',
+						Authorization: `bearer ${token}`,
 					},
 				});
 				const data = await response.json();
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,15 @@
 import { Component } from 'react';
 import { AppContext } from '../services/AppContext';
+import styles from './Home.module.css';
 
 export default class Home extends Component {
 	static contextType = AppContext;
+
+	/* TODO
+        1.  Me Card
+        2. Jobs Card
+
+    */
 
 	state = {
 		me: null,
@@ -15,18 +22,26 @@ export default class Home extends Component {
 		}
 
 		this.getMe();
+		this.getJobs();
 	}
 
 	getMe = () => {
 		const { api } = this.context;
 		api.getMe()
 			.then((me) => {
-				const { id } = me;
-				if (!id) {
-					window.location.href = '/';
-					return;
-				}
 				this.setState({ me });
+				console.log(`ME`);
+				console.log(me);
+			})
+			.catch(console.error);
+	};
+
+	getJobs = () => {
+		const { api } = this.context;
+		api.getJobs()
+			.then((jobs) => {
+				console.log(`JOBS`);
+				console.log(jobs);
 			})
 			.catch(console.error);
 	};
@@ -36,6 +51,47 @@ export default class Home extends Component {
 	};
 
 	render() {
-		return <div>Home</div>;
+		const { me } = this.state;
+		return (
+			<div className={styles['container']}>
+				<main>
+					<div>
+						<section className={styles['me-section']}>
+							<Me me={me} />
+						</section>
+						<section className={styles['jobs-section']}>
+							<div></div>
+						</section>
+					</div>
+					<div>
+						<section className={styles['map-section']}></section>
+					</div>
+				</main>
+			</div>
+		);
 	}
+}
+
+function Me({ me }) {
+	if (!me) {
+		return <div></div>;
+	}
+	const { id, name, email } = me;
+	return (
+		<div>
+			<h1>User</h1>
+			<div>
+				<label htmlFor={'id'}>ID</label>
+				<input name={'id'} disabled type={'text'} value={id} />
+			</div>
+			<div>
+				<label htmlFor={'name'}>Name</label>
+				<input name={'name'} disabled type={'text'} value={name} />
+			</div>
+			<div>
+				<label htmlFor={'email'}>Email</label>
+				<input disabled name={'email'} type={'email'} value={email} />
+			</div>
+		</div>
+	);
 }
