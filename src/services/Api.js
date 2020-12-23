480fa5bda35fb16fc566f077af254ec7103d4ec7
jjzcru <jjzcru@gmail.com>
2020-12-22 22:40:01 -0500

    Work on home

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,36 +1,38 @@
 export default class Api {
-    constructor() {
-        this.host = 'https://coding-test.rootstack.net';
-    }
+	constructor() {
+		this.host = 'https://coding-test.rootstack.net';
+	}
 	getJobs() {
 		return new Promise({});
 	}
 
 	getMe() {
-		return new Promise({});
+		return new Promise(async (resolve, reject) => {
+            resolve(true)
+        });
 	}
 
-    authenticate({email, password}) {
+	authenticate({ email, password }) {
 		return new Promise(async (resolve, reject) => {
-            try {
-                const formData = new FormData();
-                formData.append('email', email);
-                formData.append('password', password);
-                const response = await fetch(`${this.host}/api/auth/login`, {
-                    method: 'post',
-                    body: formData,
-                    mode: 'cors',
-                    redirect: "follow",
-                    referrer: "no-referrer",
-                    headers: {
-                        'Accept': '*/*',
-                    }
-                })
-                const data = await response.json();
-                resolve(data);
-            } catch(e) {
-                reject(e);
-            }
-        });
+			try {
+				const formData = new FormData();
+				formData.append('email', email);
+				formData.append('password', password);
+				const response = await fetch(`${this.host}/api/auth/login`, {
+					method: 'post',
+					body: formData,
+					mode: 'cors',
+					redirect: 'follow',
+					referrer: 'no-referrer',
+					headers: {
+						Accept: '*/*',
+					},
+				});
+				const data = await response.json();
+				resolve(data);
+			} catch (e) {
+				reject(e);
+			}
+		});
 	}
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,21 @@
 import { Component } from 'react';
+import { AppContext } from '../services/AppContext';
 
 export default class Home extends Component {
-    componentDidMount() {
-        if(!this.isAuthenticated()) {
-            window.location.href = '/auth';
-        }
+	static contextType = AppContext;
+
+	componentDidMount() {
+        
+        
+		if (!this.isAuthenticated()) {
+			window.location.href = '/auth';
+		}
     }
 
-    isAuthenticated = () => {
-        return !!localStorage.getItem('token')
-    }
+
+	isAuthenticated = () => {
+		return !!localStorage.getItem('token');
+	};
 
 	render() {
 		return <div>Home</div>;
